944ea37c24a759a1e605a702c14389f89adb36ad
Kent McGillivary <mackent@gmail.com>
2018-02-27 22:07:40 -0700

    Initial Commit

---
+++ MetalApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalApp
+//
+//  Created by Kent McGillivary on 2/27/18.
+//  Copyright © 2018 Kent McGillivary App Shop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MetalApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalApp
+//
+//  Created by Kent McGillivary on 2/27/18.
+//  Copyright © 2018 Kent McGillivary App Shop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
