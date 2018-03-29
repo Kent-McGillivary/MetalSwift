5da81714417626b3d29a852a10176f6c85a5ad40
Kent McGillivary <mackent@gmail.com>
2018-03-29 17:27:11 -0600

    Remove unused file.

--- MetalApp/MathUtilities.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MathUtilities.swift
-//  MetalApp
-//
-//  Created by Kent McGillivary on 3/28/18.
-//  Copyright © 2018 Kent McGillivary App Shop. All rights reserved.
-//
-
-import Foundation
